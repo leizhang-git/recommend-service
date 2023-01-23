191a45e3b1f0bb6d396893af312133d4dcdca8ef
leiz <vip.p@live.com>
2023-01-23 19:23:03 +0800

    补充日志信息

--- src/main/java/cn/imut/ncee/VolunteerAdministrationDemoApplication.java
+++ src/main/java/cn/imut/ncee/VolunteerAdministrationDemoApplication.java
@@ -1,14 +1,49 @@
 package cn.imut.ncee;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
 
 @MapperScan("cn.imut.ncee.dao")
 @SpringBootApplication()
 public class VolunteerAdministrationDemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(VolunteerAdministrationDemoApplication.class);
+
+    private final Environment environment;
+
+    public VolunteerAdministrationDemoApplication(Environment environment) {
+        this.environment = environment;
+    }
+
+    @PostConstruct
+    public void init() {
+        String[] pros = environment.getActiveProfiles();
+        List<String> activeProfiles = Arrays.asList(pros);
+        log.info("\n================ activeProfiles list is {}================", activeProfiles);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(VolunteerAdministrationDemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(VolunteerAdministrationDemoApplication.class, args);
+        int beanCount = ctx.getBeanDefinitionCount();
+        log.info("\n========================================= bean 数量为 ：{}", beanCount);
+        Environment env = ctx.getEnvironment();
+        String port = env.getProperty("server.port");
+        log.info("\n========================================= port is {}", port);
+        String appName = env.getProperty("spring.application.name");
+        log.info("\n========================================= application-name is {}", appName);
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n========================================= context-path is {}", path);
+        log.info("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        log.info("\n\n\t================= 项目启动成功！================\n\n");
+        log.info("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 }
