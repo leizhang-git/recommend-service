9cff6d44a19a779849d22a3ce214e14749a33d7c
leiz <vip.p@live.com>
2023-02-18 22:45:59 +0800

    补充AES加密解密工具类

--- src/main/java/cn/imut/ncee/config/Constants.java
+++ src/main/java/cn/imut/ncee/config/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String SYSTEM = "system";
 
     public static final String TOKEN = "token";
+
+    public static final String AES_KEY="494633c3-9370-b871-e625-b4310456c611";
 }
