fffe44abc3ef108b8997cd4abb7c30b8b6a4e23a
zhanglei <vip.p@live.com>
2023-06-21 15:43:56 +0800

    增加随机数工具类

--- recommend-bootstrap/src/test/java/com/recommend/bootstrap/MyTest.java
+++ recommend-bootstrap/src/test/java/com/recommend/bootstrap/MyTest.java
@@ -1,11 +1,16 @@
 package com.recommend.bootstrap;
 
+import com.recommend.bootstrap.util.RandomUtil;
+import com.recommend.consumer.dao.MajorDao;
+import com.recommend.consumer.domain.pojo.MajorInfo;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
 
 /**
  * @Auth zhanglei
@@ -18,8 +23,26 @@ public class MyTest {
     @Autowired
     private StringEncryptor stringEncryptor;
 
+    @Autowired
+    private MajorDao majorDao;
+
     @Test
     public void test() {
         String encrypt = stringEncryptor.encrypt("");
+    }
+
+    @Test
+    public void testInsert() {
+        for (int i = 0; i < 1000; i++) {
+            MajorInfo majorInfo = new MajorInfo();
+            majorInfo.setMajorId(UUID.randomUUID().toString());
+            majorInfo.setMajorName(RandomUtil.getMajor());
+            majorInfo.setMajorCode("2B");
+            majorInfo.setMajorCategory("none");
+            majorInfo.setEnrollPerson(RandomUtil.getNum(25, 120));
+            majorInfo.setUniversityId("xxx");
+            majorInfo.setSubject(RandomUtil.getNum(0, 1));
+            majorDao.insertMajorInfo(majorInfo);
+        }
     }
 }
