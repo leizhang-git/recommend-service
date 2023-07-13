15f99795b549a7008a24cc8499e207da0bbb38e4
zhanglei <vip.p@live.com>
2023-07-13 10:59:30 +0800

    对时间日期接口增加redis缓存

--- recommend-provider/src/main/java/com/recommend/provider/service/cimpl/InitServiceImpl.java
+++ recommend-provider/src/main/java/com/recommend/provider/service/cimpl/InitServiceImpl.java
@@ -2,11 +2,15 @@ package com.recommend.provider.service.cimpl;
 
 import com.recommend.consumer.service.InitService;
 import com.recommend.consumer.util.ConstantDefinitionUtil;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @auther zhanglei
@@ -14,11 +18,22 @@ import java.util.Map;
  */
 @Service
 public class InitServiceImpl implements InitService {
+
+    private static final String ALL_MACRO = "ALL_MACRO";
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Override
     public Map<String, Object> getMarcoValues(HttpServletRequest request) {
         Map<String,Object> result = new HashMap<>();
-        Map<String, String> allMacro = ConstantDefinitionUtil.getAllMacro();
-        result.putAll(allMacro);
+        Object allMacro = redisTemplate.opsForValue().get(ALL_MACRO);
+        if(ObjectUtils.isEmpty(allMacro)) {
+            allMacro = ConstantDefinitionUtil.getAllMacro();
+            redisTemplate.opsForValue().set(ALL_MACRO, allMacro);
+            redisTemplate.expire(ALL_MACRO, 60 * 60 * 24, TimeUnit.SECONDS);
+        }
+        result.putAll((Map<String, Object>) allMacro);
         return result;
     }
 }
