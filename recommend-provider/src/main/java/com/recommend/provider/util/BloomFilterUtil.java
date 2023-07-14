1617a669464660fd7bb11557f5af410414b835ab
zhanglei <vip.p@live.com>
2023-07-14 16:40:32 +0800

    提交布隆过滤器工具类

---
+++ recommend-provider/src/main/java/com/recommend/provider/util/BloomFilterUtil.java
@@ -0,0 +1,31 @@
+package com.recommend.provider.util;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * @Describe 布隆过滤器
+ * @Author zhanglei
+ * @Date 2023/7/13 11:14
+ */
+public class BloomFilterUtil {
+
+    public static void main(String[] args) {
+        // 总数量
+        int total = 1000000;
+        BloomFilter<CharSequence> bf = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), total, 0.0002);
+        // 初始化 1000000 条数据到过滤器中
+        for (int i = 0; i < total; i++) {
+            bf.put("" + i);
+        }
+        // 判断值是否存在过滤器中
+        int count = 0;
+        for (int i = 0; i < total + 10000; i++) {
+            if (bf.mightContain("" + i)) {
+                count++;
+            }
+        }
+        System.out.println("已匹配数量 " + count);
+    }
+}
